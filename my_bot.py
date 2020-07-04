db863de6b07893bf98d773993ccd88117f3e314e
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-04 12:43:49 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -39,13 +39,10 @@ class chatbot(discord.Client):
             # msg에 지정된 내용대로 메시지를 전송
             await channel.send(msg)
             return None
-        if message.content == "!과자":
-            # 현재 채널을 받아옴
-            channel = message.channel
             # 답변 내용 구성
-            msg = "과자"
+            msg = "!과자"
             # msg에 지정된 내용대로 메시지를 전송
-            await channel.send(msg)
+            await channel.send(msg+"호출")
             return None
         if message.content == "!창규":
             # 현재 채널을 받아옴
