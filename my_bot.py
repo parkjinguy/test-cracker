e53b485cc85883b5ec4baec9ba7e8cb203d24be5
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-05 23:31:27 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -58,13 +58,17 @@ class chatbot(discord.Client):
         if message.content == "!도움말":
             if message.author.dm_channel:
                 await message.author.dm_channel.send("명령어 :  !아잇, !냥이, !안녕, !팀설정, !팀결과, !패치로그, 모름, 사딸라, !레드, !크래커, !창규, 흐뭇, !냥대노,")
-                await message.author.dm_channel.send(" !냥이월급, !보리, !리브, !햄울찜, 기분좋아, !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다")
+                await message.author.dm_channel.send(" !냥이월급, !보리, !리브, !햄울찜, 기분좋아, !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다, !투표, !결과, !찬성,")
+                await message.author.dm_channel.send(" !반대, !투표종료")
                 await message.author.dm_channel.send("!팀설정은 !팀설정 인원1 인원2 인원3로 설정을 하며 무조건 짝수만 가능합니다(설정하면 자동으로 팀을 나눕니다.) ")
+                await message.author.dm_channel.send("!투표설정은 !투표 투표내용 하면 투표가 시작되며 !찬성, !반대로 투표를 합니다. ")
             elif message.author.dm_channel is None:
                 channel = await message.author.create_dm()#dm채널 없으면 추가후 전송
                 await channel.send("명령어 : !아잇, !냥이, !안녕, !팀설정, !팀결과, !패치로그, 모름, 사딸라, !레드, !크래커, !창규, 흐뭇, !냥대노,")
-                await channel.send(" !냥이월급 !리, !리브, !햄울찜, 기분좋아 !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다")
+                await channel.send(" !냥이월급 !리, !리브, !햄울찜, 기분좋아 !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다, !투표, !결과, !찬성,")
+                await channel.send("  !반대, !투표종료")
                 await channel.send("!팀설정은 !팀설정 인원1 인원2 인원3로 설정을 하며 무조건 짝수만 가능합니다(설정하면 자동으로 팀을 나눕니다.)")
+                await channel.send("!투표설정은 !투표 투표내용 하면 투표가 시작되며 !찬성, !반대로 투표를 합니다. ")
             return None
         if message.content == "!패치로그":
             if message.author.dm_channel:
