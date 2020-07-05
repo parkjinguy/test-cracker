a19ccd7bf84e491c44ccf502ba8e15c46759206a
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-05 11:00:41 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -53,12 +53,12 @@ class chatbot(discord.Client):
         if message.content == "!도움말":
             if message.author.dm_channel:
                 await message.author.dm_channel.send("명령어 :  !아잇, !냥이, !안녕, !팀설정, !팀결과, !패치로그, 모름, 사딸라, !레드, !크래커, !창규, 흐뭇, !냥대노,")
-                await message.author.dm_channel.send(" !냥이월급, !보리, !리브, !햄울찜, 기분좋아, !초코, !여긴 따뜻해, !도라이")
+                await message.author.dm_channel.send(" !냥이월급, !보리, !리브, !햄울찜, 기분좋아, !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다")
                 await message.author.dm_channel.send("!팀설정은 !팀설정 인원1 인원2 인원3로 설정을 하며 무조건 짝수만 가능합니다(설정하면 자동으로 팀을 나눕니다.) ")
             elif message.author.dm_channel is None:
                 channel = await message.author.create_dm()#dm채널 없으면 추가후 전송
                 await channel.send("명령어 : !아잇, !냥이, !안녕, !팀설정, !팀결과, !패치로그, 모름, 사딸라, !레드, !크래커, !창규, 흐뭇, !냥대노,")
-                await channel.send(" !냥이월급 !리, !리브, !햄울찜, 기분좋아 !초코, !여긴 따뜻해, !도라이")
+                await channel.send(" !냥이월급 !리, !리브, !햄울찜, 기분좋아 !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다")
                 await channel.send("!팀설정은 !팀설정 인원1 인원2 인원3로 설정을 하며 무조건 짝수만 가능합니다(설정하면 자동으로 팀을 나눕니다.)")
             return None
         if message.content == "!패치로그":
@@ -68,7 +68,7 @@ class chatbot(discord.Client):
                 await message.author.dm_channel.send("2020-07-04 스뜐끼 답변 기능이 제거되었습니다.")
                 await message.author.dm_channel.send("2020-07-05 흐뭇, !냥대노, !냥이월급, 모름, 사딸라, !레드, !크래커, !창규, !보리, !리브, !햄울찜, 기분좋아 등이 추가되었습니다.")
                 await message.author.dm_channel.send("2020-07-05 !초코, !여긴 따뜻해 등이 추가되었습니다.")
-                await message.author.dm_channel.send("2020-07-05 !초코, !여긴 따뜻해 등이 추가되었습니다.")
+                await message.author.dm_channel.send("2020-07-05 !도라이, !나루토는 전설이다.")
             elif message.author.dm_channel is None:
                 channel = await message.author.create_dm()#dm채널 없으면 추가후 전송
                 await channel.send("2020-07-03 스뜐끼봇이 추가되었습니다.")
@@ -76,6 +76,7 @@ class chatbot(discord.Client):
                 await channel.send("2020-07-04 스뜐끼 답변 기능이 제거되었습니다.")
                 await channel.send("2020-07-05 흐뭇, !냥대노, !냥이월급, 모름, 사딸라, !레드, !크래커, !창규, !보리, !리브, !햄울찜, 기분좋아 등이 추가되었습니다.")
                 await channel.send("2020-07-05 !초코, !여긴 따뜻해 등이 추가되었습니다.")
+                await channel.send("2020-07-05 !도라이, !나루토는 전설이다.")
             return None
         #욕 필터
         #mess = message.content
@@ -89,6 +90,10 @@ class chatbot(discord.Client):
         if message.content == "!도라이":
             channel = message.channel
             await channel.send("배신자!!!")
+            return None
+        if message.content == "!나루토는 전설이다":
+            channel = message.channel
+            await channel.send("미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다.. 그찐따같던 나루토가 맞나? 진짜 나루토는 전설이다..진짜옛날에 맨날나루토봘는데 왕같은존재인 호카게 되서 세계최강 전설적인 영웅이된나루토보면 진짜내가다 감격스럽고 나루토 노래부터 명장면까지 가슴울리는장면들이 뇌리에 스치면서 가슴이 웅장해진다.. 그리고 극장판 에 카카시앞에 운석날라오는 거대한 걸 사스케가 갑자기 순식간에 나타나서 부숴버리곤 개간지나게 나루토가 없다면 마을을 지킬 자는 나밖에 없다 라며 바람처럼 사라진장면은 진짜 나루토처음부터 본사람이면 안울수가없더라 진짜 너무 감격스럽고 보루토를 최근에 알았는데 미안하다.. 지금20화보는데 진짜 나루토세대나와서 너무 감격스럽고 모두어엿하게 큰거보니 내가 다 뭔가 알수없는 추억이라해야되나 그런감정이 이상하게 얽혀있다.. 시노는 말이많아진거같다 좋은선생이고..그리고 보루토왜욕하냐 귀여운데 나루토를보는것같다 성격도 닮았어 그리고버루토에 나루토사스케 둘이싸워도 이기는 신같은존재 나온다는게 사실임?? 그리고인터닛에 쳐봣는디 이거 ㄹㅇㄹㅇ 진짜팩트냐?? 저적이 보루토에 나오는 신급괴물임?ㅡ 나루토사스케 합체한거봐라 진짜 ㅆㅂ 이거보고 개충격먹어가지고 와 소리 저절로 나오더라 ;; 진짜 저건 개오지는데.. 저게 ㄹㅇ이면 진짜 꼭봐야돼 진짜 세계도 파괴시키는거아니야 .. 와 진짜 나루토사스케가 저렇게 되다니 진짜 눈물나려고했다.. 버루토그라서 계속보는중인데 저거 ㄹㅇ이냐..? 하.. ㅆㅂ 사스케 보고싶다..  진짜언제 이렇게 신급 최강들이 되었을까 옛날생각나고 나 중딩때생각나고 뭔가 슬프기도하고 좋기도하고 감격도하고 여러가지감정이 복잡하네.. 아무튼 나루토는 진짜 애니중최거명작임..")
             return None
         #사진 
         if message.content == "!아잇":
