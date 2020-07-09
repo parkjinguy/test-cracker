512167a8a7171e6766fd85174de9ba1b78ae3f0a
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-10 01:37:48 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -421,9 +421,9 @@ class chatbot(discord.Client):
             res = requests.get(APIURL, headers=headers)
             tmm=res.json()
             ac=""
-            ac="자랭\n티어 : "+repr(tmm[0]['tier'])+" "+repr(tmm[0]['rank'])+"\n 승리수 : "+repr(tmm[0]['wins']) +"\n 패배수 : "+repr(tmm[0]['losses'])+"\n"
+            ac="솔랭\n티어 : "+repr(tmm[0]['tier'])+" "+repr(tmm[0]['rank'])+"\n 승리수 : "+repr(tmm[0]['wins']) +"\n 패배수 : "+repr(tmm[0]['losses'])+"\n"
             try:
-                ac=ac+"솔랭\n티어 : "+repr(tmm[1]['tier'])+" "+repr(tmm[1]['rank'])+"\n 승리수 : "+repr(tmm[1]['wins']) +"\n 패배수 : "+repr(tmm[1]['losses'])+"\n"
+                ac=ac+"자랭\n티어 : "+repr(tmm[1]['tier'])+" "+repr(tmm[1]['rank'])+"\n 승리수 : "+repr(tmm[1]['wins']) +"\n 패배수 : "+repr(tmm[1]['losses'])+"\n"
             except:
                 ac=ac+'자랭없음'
             await channel.send(ac)
