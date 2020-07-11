26f48326fed37d40926f94ee81d1826362a160ab
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-11 21:56:22 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -84,6 +84,7 @@ class chatbot(discord.Client):
             aa="2020-07-08 햄울찜님의 요청으로 !크래커(수정), !경이(추가) 되었습니다.\n"
             aa="2020-07-10 cracker님의 요청으로 !롤.(추가) 되었습니다.\n"
             aa="2020-07-11 !롤.(수정) 되었습니다.\n"
+            aa="2020-07-11 대부분 명령어가 다른 채팅과 같이 사용하게(수정) 되었습니다.\n"
             if message.author.dm_channel:
                 await message.author.dm_channel.send(aa)
             elif message.author.dm_channel is None:
@@ -108,61 +109,61 @@ class chatbot(discord.Client):
             await channel.send("미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다.. 그찐따같던 나루토가 맞나? 진짜 나루토는 전설이다..진짜옛날에 맨날나루토봘는데 왕같은존재인 호카게 되서 세계최강 전설적인 영웅이된나루토보면 진짜내가다 감격스럽고 나루토 노래부터 명장면까지 가슴울리는장면들이 뇌리에 스치면서 가슴이 웅장해진다.. 그리고 극장판 에 카카시앞에 운석날라오는 거대한 걸 사스케가 갑자기 순식간에 나타나서 부숴버리곤 개간지나게 나루토가 없다면 마을을 지킬 자는 나밖에 없다 라며 바람처럼 사라진장면은 진짜 나루토처음부터 본사람이면 안울수가없더라 진짜 너무 감격스럽고 보루토를 최근에 알았는데 미안하다.. 지금20화보는데 진짜 나루토세대나와서 너무 감격스럽고 모두어엿하게 큰거보니 내가 다 뭔가 알수없는 추억이라해야되나 그런감정이 이상하게 얽혀있다.. 시노는 말이많아진거같다 좋은선생이고..그리고 보루토왜욕하냐 귀여운데 나루토를보는것같다 성격도 닮았어 그리고버루토에 나루토사스케 둘이싸워도 이기는 신같은존재 나온다는게 사실임?? 그리고인터닛에 쳐봣는디 이거 ㄹㅇㄹㅇ 진짜팩트냐?? 저적이 보루토에 나오는 신급괴물임?ㅡ 나루토사스케 합체한거봐라 진짜 ㅆㅂ 이거보고 개충격먹어가지고 와 소리 저절로 나오더라 ;; 진짜 저건 개오지는데.. 저게 ㄹㅇ이면 진짜 꼭봐야돼 진짜 세계도 파괴시키는거아니야 .. 와 진짜 나루토사스케가 저렇게 되다니 진짜 눈물나려고했다.. 버루토그라서 계속보는중인데 저거 ㄹㅇ이냐..? 하.. ㅆㅂ 사스케 보고싶다..  진짜언제 이렇게 신급 최강들이 되었을까 옛날생각나고 나 중딩때생각나고 뭔가 슬프기도하고 좋기도하고 감격도하고 여러가지감정이 복잡하네.. 아무튼 나루토는 진짜 애니중최거명작임..")
             return None
         #사진 
-        if message.content == "!아잇":
+        if "!아잇" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"11.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!햄프":
+        if "!햄프" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"2.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!냥이":
+        if "!냥이" in message.content:
             channel = message.channel
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"3.jpg")
             await message.channel.send(file=file)
             await channel.send("언제와!!!")
             return None
-        if message.content == "!찌릿":
+        if "!찌릿" in message.content:
             channel = message.channel
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"21.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!냥":
+        if "!냥" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"20.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!레드":
+        if "!레드" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"6.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!잠":
+        if "!잠" in message.content:
             channel = message.channel
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"22.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!크래커":
+        if "!크래커" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"5.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!보리":
+        if "!보리" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"7.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!리브":
+        if "!리브" message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"8.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!창규":
+        if "!창규" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"4.jpg")
             await message.channel.send(file=file)
@@ -187,12 +188,12 @@ class chatbot(discord.Client):
             file=discord.File(dirctory+"14.gif")
             await message.channel.send(file=file)
             return None                          
-        if message.content == "!햄울찜":
+        if "!햄울찜" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"10.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!냥대노":
+        if "!냥대노" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"16.jpg")
             await message.channel.send(file=file)
@@ -202,47 +203,47 @@ class chatbot(discord.Client):
             file=discord.File(dirctory+"15.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!초코":
+        if "!초코" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"17.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!여긴 따뜻해":
+        if "!여긴 따뜻해" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"18.jpg")
             await message.channel.send(file=file)
             return None
-        if message.content == "!일해":
+        if "!일해" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a1.gif")
             await message.channel.send(file=file)
             return None
-        if message.content == "!죄송":
+        if "!죄송" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a2.jpg")
             await message.channel.send(file=file)
             return None
-        if  message.content == "다이어트":
+        if "!다이어트" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a3.png")
             await message.channel.send(file=file)
             return None
-        if  message.content == "!요염":
+        if "!요염" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a4.jpg")
             await message.channel.send(file=file)
             return None
-        if  message.content == "!뭘 꼬라봐":
+        if "!뭘 꼬라봐" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a5.jpg")
             await message.channel.send(file=file)
             return None
-        if  message.content == "!사륜안":
+        if "!사륜안" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a6.jpg")
             await message.channel.send(file=file)
             return None
-        if  message.content == "!경이":
+        if "!경이" in message.content:
             dirctory = os.path.dirname(__file__)
             file=discord.File(dirctory+"a7.jpg")
             await message.channel.send(file=file)
