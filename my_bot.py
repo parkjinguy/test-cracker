713aa6c222932f69e5269dc9d59b639b7e9ec957
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-18 19:43:11 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -435,7 +435,7 @@ class chatbot(discord.Client):
             channel = message.channel
             arr_str = str(message.content).split(".")
             summonerName = arr_str[1]
-            kie="RGAPI-98c5d3b1-0c8e-4e56-91f5-d3ae178c09a2"
+            kie="RGAPI-3791d24d-fc42-4c2d-bf78-f7a821a6c207"
             APIURL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + arr_str[1]
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
@@ -477,7 +477,7 @@ class chatbot(discord.Client):
             channel = message.channel
             arr_str = str(message.content).split(".")
             summonerName = arr_str[1]
-            kie="RGAPI-98c5d3b1-0c8e-4e56-91f5-d3ae178c09a2"
+            kie="RGAPI-3791d24d-fc42-4c2d-bf78-f7a821a6c207"
             APIURL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + arr_str[1]
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
