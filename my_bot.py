9eb8084c54f20f79995ebb5a8dd3d298784802a9
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-07-28 23:53:08 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -32,34 +32,34 @@ class chatbot(discord.Client):
         # 준비가 완료되면 콘솔 창에 "READY!"라고 표시
         print("READY")
     # 봇에 메시지가 오면 수행 될 액션
-    try:
-        async def on_message(self, message):
+    async def on_message(self, message):
+        try:
         # SENDER가 BOT일 경우 반응을 하지 않도록 한다.
             if message.author.bot:
                 return None
-        # message.content = message의 내용
+            # message.content = message의 내용
             if message.content == "!바보":
-            # 현재 채널을 받아옴
+                # 현재 채널을 받아옴
                 channel = message.channel
-            # 답변 내용 구성
+                # 답변 내용 구성
                 msg = "너도 바보"
-            # msg에 지정된 내용대로 메시지를 전송
+                # msg에 지정된 내용대로 메시지를 전송
                 await channel.send(msg)
                 return None;
             if message.content == "!안녕":
-            # 현재 채널을 받아옴
+                # 현재 채널을 받아옴
                 channel = message.channel
-            # 답변 내용 구성
+                # 답변 내용 구성
                 msg = "연습중인 스뜐기입니다."
-            # msg에 지정된 내용대로 메시지를 전송
+                # msg에 지정된 내용대로 메시지를 전송
                 await channel.send(msg)
-            return None
-        #메시지 해당 사람 태그 호출
-        '''if message.content == "과자":
-            channel = message.channel;
-            msg = "<@{}>".format("#9522")
-            await client.send_message(channel, "<@creake>")
-            return None'''
+                return None
+            #메시지 해당 사람 태그 호출
+            '''if message.content == "과자":
+                channel = message.channel;
+                msg = "<@{}>".format("#9522")
+                await client.send_message(channel, "<@creake>")
+                return None'''
             if message.content == "!도움말":
                 aa="명령어 :  !아잇, !냥이, !안녕, !팀설정, !팀결과, !패치로그 !찌릿, !잠, !일해, !레드, !크래커, !냥, !창규, !냥대노, !죄송\n"
                 aa= aa+" !냥이월급날, !보리, !리브, !햄울찜, 기분좋아, !초코, !여긴 따뜻해, !도라이, !나루토는 전설이다, !투표, !투표결과, !찬성,\n"
@@ -107,15 +107,15 @@ class chatbot(discord.Client):
                     channel = await message.author.create_dm()#dm채널 없으면 추가후 전송
                     await channel.send(aa)
                 return None
-        #욕 필터
-        #mess = message.content
-        #tmp = ["씨발","ㅅㅂ","시발","병신","좆까","니얼굴","니 얼굴"]
-        #for i in tmp:
-            #bad = mess.find(i)
-            #print(bad)
-            #if bad == 0:
-                #await message.channel.send("욕하지 마세요")
-                #await message.delete() 현재 관리자로 인해 비활성화
+            #욕 필터
+            #mess = message.content
+            #tmp = ["씨발","ㅅㅂ","시발","병신","좆까","니얼굴","니 얼굴"]
+            #for i in tmp:
+                #bad = mess.find(i)
+                #print(bad)
+                #if bad == 0:
+                    #await message.channel.send("욕하지 마세요")
+                    #await message.delete() 현재 관리자로 인해 비활성화
             if "!도라이" in message.content:
                 channel = message.channel
                 await channel.send("배신자!!!")
@@ -124,7 +124,7 @@ class chatbot(discord.Client):
                 channel = message.channel
                 await channel.send("미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다.. 그찐따같던 나루토가 맞나? 진짜 나루토는 전설이다..진짜옛날에 맨날나루토봘는데 왕같은존재인 호카게 되서 세계최강 전설적인 영웅이된나루토보면 진짜내가다 감격스럽고 나루토 노래부터 명장면까지 가슴울리는장면들이 뇌리에 스치면서 가슴이 웅장해진다.. 그리고 극장판 에 카카시앞에 운석날라오는 거대한 걸 사스케가 갑자기 순식간에 나타나서 부숴버리곤 개간지나게 나루토가 없다면 마을을 지킬 자는 나밖에 없다 라며 바람처럼 사라진장면은 진짜 나루토처음부터 본사람이면 안울수가없더라 진짜 너무 감격스럽고 보루토를 최근에 알았는데 미안하다.. 지금20화보는데 진짜 나루토세대나와서 너무 감격스럽고 모두어엿하게 큰거보니 내가 다 뭔가 알수없는 추억이라해야되나 그런감정이 이상하게 얽혀있다.. 시노는 말이많아진거같다 좋은선생이고..그리고 보루토왜욕하냐 귀여운데 나루토를보는것같다 성격도 닮았어 그리고버루토에 나루토사스케 둘이싸워도 이기는 신같은존재 나온다는게 사실임?? 그리고인터닛에 쳐봣는디 이거 ㄹㅇㄹㅇ 진짜팩트냐?? 저적이 보루토에 나오는 신급괴물임?ㅡ 나루토사스케 합체한거봐라 진짜 ㅆㅂ 이거보고 개충격먹어가지고 와 소리 저절로 나오더라 ;; 진짜 저건 개오지는데.. 저게 ㄹㅇ이면 진짜 꼭봐야돼 진짜 세계도 파괴시키는거아니야 .. 와 진짜 나루토사스케가 저렇게 되다니 진짜 눈물나려고했다.. 버루토그라서 계속보는중인데 저거 ㄹㅇ이냐..? 하.. ㅆㅂ 사스케 보고싶다..  진짜언제 이렇게 신급 최강들이 되었을까 옛날생각나고 나 중딩때생각나고 뭔가 슬프기도하고 좋기도하고 감격도하고 여러가지감정이 복잡하네.. 아무튼 나루토는 진짜 애니중최거명작임..")
                 return None
-        #사진 
+            #사진 
             if "!아잇" in message.content:
                 dirctory = os.path.dirname(__file__)
                 file=discord.File(dirctory+"11.jpg")
@@ -329,7 +329,7 @@ class chatbot(discord.Client):
                 await message.channel.send(file=file)
                 return None
             #기능 부분
-
+            
             global ii
             global count
             if "!사다리 " in message.content:
@@ -386,7 +386,7 @@ class chatbot(discord.Client):
                 count=0
                 return None
             #투표
-
+            
             if "!투표 " in message.content:
                 channel = message.channel
                 arr_str = str(message.content).split(" ")
@@ -394,7 +394,7 @@ class chatbot(discord.Client):
                 tm = "현재 "+topo[0]+"의 투표가 추가되었습니다."
                 await channel.send(tm)
                 await channel.send("!찬성, !반대를 해주세요")
-
+                
             if message.content == "!찬성":
                 channel = message.channel
                 ac=0
@@ -447,7 +447,7 @@ class chatbot(discord.Client):
                             print("fs")
                             topo2.append(message.author.id)
                             return None
-
+                        
                 except:
                     await channel.send("진행중인 투표가 없습니다.")
             if message.content == "!투표 종료":
@@ -456,7 +456,7 @@ class chatbot(discord.Client):
                 topo.clear()
                 topo1.clear()
                 topo2.clear()
-
+                
             if message.content == "!투표결과":
                 channel = message.channel
                 try:
@@ -475,7 +475,7 @@ class chatbot(discord.Client):
                 topo.clear()
                 topo1.clear()
                 topo2.clear()
-
+                
             if "!롤." in message.content:
                 channel = message.channel
                 arr_str = str(message.content).split(".")
