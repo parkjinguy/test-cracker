398fe17ba7f03b66cb12304d27c75d53eb0f860f
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-08-01 23:54:05 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,4 @@
-if "!롤." in message.content:
-            channel = message.channel
-            arr_str = str(message.content).split(".")
-            summonerName = arr_str[1]
-            kie="RGAPI-32974447-b52e-4c46-a035-8a946a3615ed"
-            APIURL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + arr_str[1]
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-                "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                "Origin": "https://developer.riotgames.com",
-                "X-Riot-Token": kie
-                }
-            res = requests.get(APIURL, headers=headers)
-            tmm=res.json()
-            id=tmm['id']
-            summonerName=id
-            APIURL = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-                "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                "Origin": "https://developer.riotgames.com",
-                "X-Riot-Token": kie
-                }
-            res = requests.get(APIURL, headers=headers)
-            tmm=res.json()
-            ac=""
-            try:
-                if tmm[0]['queueType']=="RANKED_FLEX_SR":
-                    ac=ac+"자랭"
-                else:
-                    ac=ac+"솔랭"
-                ac=ac+"\n티어 : "+repr(tmm[0]['tier'])+" "+repr(tmm[0]['rank'])+"\n 승리수 : "+repr(tmm[0]['wins']) +"\n 패배수 : "+repr(tmm[0]['losses'])+"\n점수"+repr(tmm[0]['leaguePoints'])+"\n"
-                if tmm[1]['queueType']=="RANKED_FLEX_SR":
-                    ac=ac+"자랭"
-                else:
-                    ac=ac+"솔랭"
-                ac=ac+"\n티어 : "+repr(tmm[1]['tier'])+" "+repr(tmm[1]['rank'])+"\n 승리수 : "+repr(tmm[1]['wins']) +"\n 패배수 : "+repr(tmm[1]['losses'])+"\n점수"+repr(tmm[1]['leaguePoints'])+"\n"
-            except:
-                ac=ac+'랭크없음'
-            await channel.send(ac)
-        if "!최근롤." in message.content:
+if "!최근롤." in message.content:
             channel = message.channel
             arr_str = str(message.content).split(".")
             summonerName = arr_str[1]
