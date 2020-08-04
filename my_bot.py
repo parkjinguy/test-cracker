18ee850aa36fdb1bd8f868a0636602d8b6bc4de7
parkjinguy <55516987+parkjinguy@users.noreply.github.com>
2020-08-05 01:21:16 +0900

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -336,39 +336,29 @@ class chatbot(discord.Client):
                 row=curs.fetchone()
             await channel.send(tmp)
             return
-        if "!참여." in message.content:
+        if "!참가입력." in message.content:
             channel = message.channel
             conn = pymysql.connect(host='axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', user='jji99uozpvwxyq5v', password='wvombycad95zjtqp',db='ar5orud2kuyhu3gv', charset='utf8')
             curs = conn.cursor()
             arr = str(message.content).split(".")
-            sql = "select * from cat1 where name='"+arr[1]+"'"
-            curs.execute(sql)
-            try:
-                row = curs.fetchone()
-                str(row[0])
-                conn.close()
-                conn = pymysql.connect(host='axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', user='jji99uozpvwxyq5v', password='wvombycad95zjtqp',db='ar5orud2kuyhu3gv', charset='utf8')
-                curs = conn.cursor()
-                sql = "select name cat2 where name='"+str(message.author.name)+"'"
+            if "Cracker" == message.author.name or "냥이" == message.author.name:
+                sql = "select * from cat1 where name='"+arr[1]+"'"
                 curs.execute(sql)
-                row = curs.fetchone()
                 try:
+                    row = curs.fetchone()
                     str(row[0])
                     conn.close()
-                    await channel.send("이미 참가자 입니다.")
-                    return
-                except:
                     conn = pymysql.connect(host='axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', user='jji99uozpvwxyq5v', password='wvombycad95zjtqp',db='ar5orud2kuyhu3gv', charset='utf8')
                     curs = conn.cursor()
-                    sql = "insert into cat2 values("+str(row[0])+",'"+str(message.author.name)+"')"
+                    sql = "insert into cat2 values("+str(row[0])+",'"+str(arr[2])+"')"
                     curs.execute(sql)
                     conn.commit()
                     conn.close()
-                    await channel.send(message.author.name+"님이 "+str(row[1])+"에 참여 하였습니다.")
+                    await channel.send(arr[2]+"님이 "+str(row[1])+"에 참여 하였습니다.")
                     return
-            except:
-                await channel.send("참여 목록에 없습니다.")
-                return
+                except:
+                    await channel.send("이미 참가자 입니다.")
+                    return
             return
         if "!참여인원." in message.content:
             channel = message.channel
